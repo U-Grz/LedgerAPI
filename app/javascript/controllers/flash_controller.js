9d471a365ea582453993cd677ed058d81fd4233f
Ugnius <ugnius.grazulis@etztec.com>
2026-02-04 22:52:10 +0000

    Edit Transaction

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,15 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller{
-	static targets = ["message"]
+export default class extends Controller {
+  static targets = ["message"]
 
-	connect(){
-		this.timeout = setTimeout(() =>{
-			this.dismiss()
-		}, 5000)
-	}
+  connect() {
+    // Auto-dismiss after 5 seconds
+    this.timeout = setTimeout(() => {
+      this.dismiss()
+    }, 5000)
+  }
 
-	disconnect(){
-		this.element.remove()
-	}
+  dismiss() {
+    // Clear the timeout and remove the element
+    clearTimeout(this.timeout)
+    this.element.remove()
+  }
+
+  disconnect() {
+    // Clean up timeout when controller is disconnected
+    clearTimeout(this.timeout)
+  }
 }
