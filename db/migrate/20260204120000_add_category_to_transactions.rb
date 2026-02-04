3e782df266a13261dda6d00c2aec334d05b67236
Ugnius <ugnius.grazulis@etztec.com>
2026-02-04 21:10:57 +0000

    Column add to db

--- db/migrate/20260204120000_add_category_to_transactions.rb
+++ db/migrate/20260204120000_add_category_to_transactions.rb
@@ -1,4 +1,4 @@
-class AddCategoryToTransactions < AvtiveRecord::Migration[8.1]
+class AddCategoryToTransactions < ActiveRecord::Migration[8.1]
 	def change
 		add_column :transactions, :category, :string
 	end
