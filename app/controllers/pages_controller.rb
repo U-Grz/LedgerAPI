e478a88454fc7ee007a711f8a68c2ebf97df313b
U-Grz <ugnius.grazulis@etztec.com>
2026-02-04 15:09:14 +0000

    syntax fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController
-  
   def home
     # If user is already logged in, send them to dashboard
     if session[:user_id] && User.find_by(id: session[:user_id])
@@ -7,4 +6,13 @@ class PagesController < ApplicationController
     end
     # Otherwise, show home page
   end
+  
+  private
+  
+  # Make current_user available in views
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  
+  helper_method :current_user
 end
